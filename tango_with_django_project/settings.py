4ef9f70439887a53a154b466bc5b2ec292a8f512
Kai Loach <3002935l@student.gla.ac.uk>
2026-02-09 17:37:15 +0000

    Chapter 9 complete

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -95,6 +95,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS':{'min_length':6,}
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -130,3 +131,12 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+#password
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    )
+
+LOGIN_URL = 'rango:login'
